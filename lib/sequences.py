0aaf590f234be531776e16973cc7b909f5fe250d
Dennis Kibet <dennisrkibet@gmail.com>
2024-09-01 10:53:12 +0300

    implemented fibonacci sequence and passed the unit tests

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    if length == 0:
+        print([])
+        return
+    elif length == 1:
+        print([0])
+        return
+
+    # Start the list with the first two Fibonacci numbers
+    fibonacci_list = [0, 1]
+
+    # Generate the Fibonacci sequence until it reaches the desired length
+    while len(fibonacci_list) < length:
+        # Add the sum of the last two elements to the list
+        fibonacci_list.append(fibonacci_list[-1] + fibonacci_list[-2])
+
+    print(fibonacci_list)
